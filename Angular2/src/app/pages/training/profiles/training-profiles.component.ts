d3c59552cf1d07900bc50c9a6076b2084dafda55
Philipp <philipp.eisenmann@fielmann.com>
2023-04-19 15:24:38 +0200

    fix: Angular build errors

--- Angular2/src/app/pages/training/profiles/training-profiles.component.ts
+++ Angular2/src/app/pages/training/profiles/training-profiles.component.ts
@@ -8,17 +8,18 @@ import { NgxSpinnerService } from 'ngx-spinner';
 @Component({
   selector: 'app-training-profiles',
   templateUrl: './training-profiles.component.html',
-  styleUrls: ['./training-profiles.component.scss'],
+  styleUrls: ['./training-profiles.component.scss']
+})
 
 export class TrainingProfilesComponent implements OnInit, OnDestroy {
-    public profiles: Profile[] = [];
-    private subscriptions: Subscription[] = [];
+  public profiles: Profile[] = [];
+  private subscriptions: Subscription[] = [];
 
   constructor(
     private trainingService: TrainingService,
     private router: Router,
     private spinner: NgxSpinnerService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.spinner.show();
@@ -31,8 +32,8 @@ export class TrainingProfilesComponent implements OnInit, OnDestroy {
   }
 
   onSelectProfile(profile: Profile) {
-        this.router.navigate(['training', 'profile', profile.id]);
-    }
+    this.router.navigate(['training', 'profile', profile.id]);
+  }
 
   ngOnDestroy(): void {
     this.subscriptions.forEach((sub) => {
@@ -41,4 +42,4 @@ export class TrainingProfilesComponent implements OnInit, OnDestroy {
       }
     });
   }
-
+}
